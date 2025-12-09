d9576bfacc6fb516105e34e5ee276e2c2a9f7f91
owenritchie <owenrit97@gmail.com>
2025-12-09 18:06:45 -0500

    more slq changes to dbt model

--- dbt_pipelines/models/oc_transpo/staging/stg_active_vehicles.sql
+++ dbt_pipelines/models/oc_transpo/staging/stg_active_vehicles.sql
@@ -1,16 +1,16 @@
 {{ config(materialized='view') }}
 
 SELECT
-    CAST("vehicle.vehicle.id" AS INTEGER) AS vehicle_id,
-    CAST("vehicle.trip.route_id" AS VARCHAR) || '-' || CAST("vehicle.trip.direction_id" AS VARCHAR) AS route_id,
-    "vehicle.trip.trip_id" AS trip_id,
-    "vehicle.trip.start_time" AS scheduled_start_time,
-    "vehicle.trip.start_date" AS scheduled_start_date,
-    CAST("vehicle.position.latitude" AS DECIMAL(10, 8)) AS latitude,
-    CAST("vehicle.position.longitude" AS DECIMAL(11, 8)) AS longitude,
-    CAST("vehicle.position.bearing" AS DECIMAL(5, 2)) AS bearing,
-    CAST("vehicle.position.speed" AS DECIMAL(6, 2)) AS speed,
-    CAST("vehicle.current_status" AS INTEGER) AS current_status,
-    "vehicle.timestamp" AS last_update_timestamp
+    CAST("Vehicle.Vehicle.Id" AS INTEGER) AS vehicle_id,
+    CAST("Vehicle.Trip.RouteId" AS VARCHAR) || '-' || CAST("Vehicle.Trip.DirectionId" AS VARCHAR) AS route_id,
+    "Vehicle.Trip.TripId" AS trip_id,
+    "Vehicle.Trip.StartTime" AS scheduled_start_time,
+    "Vehicle.Trip.StartDate" AS scheduled_start_date,
+    CAST("Vehicle.Position.Latitude" AS DECIMAL(10, 8)) AS latitude,
+    CAST("Vehicle.Position.Longitude" AS DECIMAL(11, 8)) AS longitude,
+    CAST("Vehicle.Position.Bearing" AS DECIMAL(5, 2)) AS bearing,
+    CAST("Vehicle.Position.Speed" AS DECIMAL(6, 2)) AS speed,
+    CAST("Vehicle.CurrentStatus" AS INTEGER) AS current_status,
+    "Vehicle.Timestamp" AS last_update_timestamp
 FROM {{ source('raw', 'raw_active_vehicles') }}
-WHERE "vehicle.trip.route_id" IS NOT NULL
+WHERE "Vehicle.Trip.RouteId" IS NOT NULL
