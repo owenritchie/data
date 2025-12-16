ba9500c68d391266048f800a2e42914a9a420ab9
Owen Ritchie <100958189+owenritchie@users.noreply.github.com>
2025-12-16 14:36:44 -0500

    Change materialization to incremental for weather data

--- dbt_pipelines/models/open_meteo/presentation/fct_current_weather.sql
+++ dbt_pipelines/models/open_meteo/presentation/fct_current_weather.sql
@@ -1,9 +1,24 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='incremental',
+    unique_key='snapshot_timestamp'
+) }}
 
-SELECT
-    temperature_celsius,
-    precipitation_mm,
-    weather_description,
-    weather_detailed,
-    CURRENT_TIMESTAMP AT TIME ZONE 'America/Toronto' AS updated_at
-FROM {{ ref('stg_current_weather') }}
+with weather_snapshot as (
+
+    select
+        temperature_celsius,
+        precipitation_mm,
+        weather_description,
+        weather_detailed,
+        CURRENT_TIMESTAMP AT TIME ZONE 'America/Toronto' as snapshot_timestamp
+    from {{ ref('stg_current_weather') }}
+
+)
+
+select *
+from weather_snapshot
+
+{% if is_incremental() %}
+where snapshot_timestamp >
+    (select max(snapshot_timestamp) from {{ this }})
+{% endif %}
