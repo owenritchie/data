36f2cd87820e39d4f151efbbead106f87597b21e
owenritchie <owenrit97@gmail.com>
2025-12-16 17:46:52 -0500

    fixed fact table

--- dbt_pipelines/models/open_meteo/presentation/fct_current_weather.sql
+++ dbt_pipelines/models/open_meteo/presentation/fct_current_weather.sql
@@ -3,22 +3,14 @@
     unique_key='snapshot_timestamp'
 ) }}
 
-with weather_snapshot as (
-
-    select
-        temperature_celsius,
-        precipitation_mm,
-        weather_description,
-        weather_detailed,
-        CURRENT_TIMESTAMP AT TIME ZONE 'America/Toronto' as snapshot_timestamp
-    from {{ ref('stg_current_weather') }}
-
-)
-
-select *
-from weather_snapshot
+select
+    temperature_celsius,
+    precipitation_mm,
+    weather_description,
+    weather_detailed,
+    CURRENT_TIMESTAMP AT TIME ZONE 'America/Toronto' as snapshot_timestamp
+from {{ ref('stg_current_weather') }}
 
 {% if is_incremental() %}
-where snapshot_timestamp >
-    (select max(snapshot_timestamp) from {{ this }})
+where CURRENT_TIMESTAMP AT TIME ZONE 'America/Toronto' > (select max(snapshot_timestamp) from {{ this }})
 {% endif %}
